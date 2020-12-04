51bf0322b866753f49b72ee073169c3e7a9771fa
Artem Stepanenko <artem@stepanenko.one>
2020-12-04 16:44:52 +0200

    add loading ALLOWED_HOSTS from env vars

--- simpletodo/settings.py
+++ simpletodo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SIMPLETODO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('SIMPLETODO_DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 # Application definition
