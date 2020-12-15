2f581735d6768615ad20816a73409a2ba47683ed
Artem Stepanenko <artem@stepanenko.one>
2020-12-15 16:32:47 +0200

    add splitting to ALLOWED_HOSTS

--- simpletodo/settings/common.py
+++ simpletodo/settings/common.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split()
 
 SECRET_KEY = os.getenv('SIMPLETODO_SECRET_KEY')
 # Application definition
