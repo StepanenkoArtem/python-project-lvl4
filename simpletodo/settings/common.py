aa4d2a9700cc29a2e4cd1d7b6ee3da3c8198d4f2
Artem Stepanenko <artem@stepanenko.one>
2021-01-09 18:55:37 +0200

    add loadenv to settings/common.py

--- simpletodo/settings/common.py
+++ simpletodo/settings/common.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+# Import environment variable from .env file
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
