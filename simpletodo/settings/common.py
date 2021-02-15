72c5abbbf53baf78732b6201f353cf232bbcd87e
Artem Stepanenko <artem@stepanenko.one>
2021-02-15 16:52:14 +0200

    rewrite STATIC_ROOT definition

--- simpletodo/settings/common.py
+++ simpletodo/settings/common.py
@@ -111,7 +111,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 THEMES_DIR = 'themes'
 
 STATICFILES_DIRS = [
